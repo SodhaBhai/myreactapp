80e794463ac993b78ad376b4187a0d73db1223eb
Kuldeep Singh Sodha <kuldeep_komal@Kuldeeps-MacBook-Air.local>
2024-10-26 14:11:49 +0530

    state of object

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import GoalList from './components/GoalList';
-
+import GoalList from './components/GoalLists/GoalList';
+import NewGoal from './components/NewGoals/NewGoal';
 const App = () => {
+   const [courseGoals,setCourseGoals] = useState([
+      {id: 'cg1' , text: 'Kuldeep'},
+      {id: 'cg2' , text: 'Singh'},
+      {id: 'cg3' , text: 'Sodha'},
+    ]);
 
-  const courseGoals = [
-    {id: 'cg1' , text: 'Kuldeep'},
-    {id: 'cg2' , text: 'Singh'},
-    {id: 'cg3' , text: 'Sodha'},
-  ];
+  const addNewGoalHandler = newGoal =>{
+   setCourseGoals(courseGoals.concat(newGoal))
+   //if our state depends on prev state than go with bellow logic
+   //setCourseGoals(courseGoals => courseGoals.concat(newGoal))
+  }
 
-  return <div>
-    <h2>Course Goal</h2>
-    <GoalList goals={courseGoals}/>
-  </div>;
+  return (
+    <div className="course-goals">
+      <h2>Course Goal</h2>
+      <NewGoal onAddGoal={addNewGoalHandler}/>
+      <GoalList goals={courseGoals}/>
+    </div>
+  );
 };
 
 export default App;
