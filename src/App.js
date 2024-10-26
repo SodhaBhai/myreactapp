345eac0f21e616b43c6580f2d7fd11ac7b680fa3
Kuldeep Singh Sodha <kuldeep_komal@Kuldeeps-MacBook-Air.local>
2024-10-26 10:49:36 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return <div>
+    <h2>Course Goal</h2>
+    <ul className="goal-list">
+      <li>Finish the course</li>
+      <li>Learn all about the course</li>
+      <li>Help our student in the Course</li>
+    </ul>
+  </div>;
+};
+
+export default App;
